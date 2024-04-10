79f810666621f7087196770b8275d2a47ee6c8a5
Mac-zyzds <Mac-zyzds>
2024-04-10 19:54:56 +0800

    added lua/core/keymaps.lua and nvim-telescope

--- init.lua
+++ init.lua
@@ -1,4 +1,6 @@
-require("core.config")
+vim.g.mapleader = " "       --主键: 空格
 require("core.lazy")
+require("core.config")
+require("core.keymaps")
 
 vim.cmd [[colorscheme tokyonight-moon]]
--- lua/core/config.lua
+++ lua/core/config.lua
@@ -1,2 +1,34 @@
+-- number
 vim.opt.relativenumber = true
 vim.opt.number = true
+
+-- tab
+vim.opt.tabstop = 4		-- 显示缩进符时的缩进数
+vim.opt.shiftwidth = 4		-- 按下tab插入的缩进数
+vim.opt.expandtab = true        -- 使用空格代替制表符进行缩进
+vim.opt.autoindent = true      	-- 自动缩进
+
+--防止包裹
+vim.opt.wrap = false
+
+-- 光标
+vim.opt.cursorline = true
+
+-- 启用鼠标
+vim.opt.mouse:append("a")
+
+-- 系统剪切板
+vim.opt.clipboard:append("unnamedplus")
+
+-- 默认新窗口右和下
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- 搜索
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- 外观
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
+
