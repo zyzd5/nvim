ead8898a29f116a2a43dc553eb7cb3ce3d44498f
Mac-zyzds <Mac-zyzds>
2024-04-15 03:38:46 +0800

    added Mason but no lsp plugins, make config butter

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -11,4 +11,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath) 
 
-require("lazy").setup({{import = "plugins"}})
+require("lazy").setup({
+    {import = "plugins"},
+    {import = "plugins/lsp"},
+})
--- lua/plugins/nvim-telescope.lua
+++ lua/plugins/nvim-telescope.lua
@@ -1,6 +1,7 @@
 return 
 {
     'nvim-telescope/telescope.nvim', tag = '0.1.6',
+      event = "VeryLazy",
       dependencies = { 'nvim-lua/plenary.nvim' },
       config = function()
             require('telescope').setup()
