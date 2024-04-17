486b1b82dab32fbc291b66f4c78408ed7b572525
Mac-zyzds <Mac-zyzds>
2024-04-17 21:23:53 +0800

    added Dashboard-nvim

---
+++ lua/plugins/dashboard-nvim.lua
@@ -0,0 +1,20 @@
+return
+{
+  'nvimdev/dashboard-nvim',
+  event = 'VimEnter',
+  config = function()
+    require('dashboard').setup {
+    theme = 'hyper',
+    config = {
+      week_header = {
+       enable = true,
+      },
+      shortcut = {
+        { desc = ' Update', group = '@property', action = 'Lazy update', key = 'u' },
+        { desc = ' Lazy', group = '@property', action = 'Lazy', key = 'L'},
+        { icon = ' ', icon_hl = '@variable', desc = 'Files', group = 'Label', action = 'Telescope find_files', key = 'f' },
+      },
+    }
+            }end,
+  dependencies = {{'nvim-tree/nvim-web-devicons'}}
+}
