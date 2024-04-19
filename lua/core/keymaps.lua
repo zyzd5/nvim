3fa909902e34b97f5e486a419393ae11bdd662e8
Mac-zyzds <Mac-zyzds>
2024-04-19 14:02:00 +0800

    update keymap

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,1 +1,1 @@
-vim.keymap.set({"n"}, "<leader>F", [[<cmd>lua vim.lsp.buf.format {async = true}<CR>]]) 
+vim.keymap.set({"n"}, "<leader>F", [[<cmd>lua vim.lsp.buf.format {async = true}<CR>]], {desc = "format code"}) 
--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -6,7 +6,7 @@ return
     dependencies = 'nvim-tree/nvim-web-devicons',
     keys = {
         { "<leader>w",     ":BufferLinePickClose<CR>", desc = "Pick a buffer close", silent = true },
-        { "<leader><tab>", ":BufferLineMoveNext<CR>",  desc = "Move to next buffer",  silent = true },
+        { "<leader><tab>", ":BufferLineCycleNext<CR>",  desc = "Move to next buffer",  silent = true },
     },
     config = function()
         require('bufferline').setup({})
