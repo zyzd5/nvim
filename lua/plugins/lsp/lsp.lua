9e2306086a21fc3a6f79e680a9dab6664e972084
Mac-zyzds <Mac-zyzds>
2024-04-19 14:36:38 +0800

    more lsp config, but still not figure it out

--- lua/plugins/lsp/lsp.lua
+++ lua/plugins/lsp/lsp.lua
@@ -1,40 +1,61 @@
 return
 {
     "neovim/nvim-lspconfig",
-    dependencies = {
-            "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig",
+    cmd = {
+            "Mason",
+            "Neoconf",
         },
+    dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig",
+        "folke/neoconf.nvim",
+        "folke/neodev.nvim",
+        {
+            "j-hui/fidget.nvim",
+            tag = "legacy",
+        },
+        "nvimdev/lspsaga.nvim",
+    },
     config = function()
         local servers = {
-                lua_ls = {
+            lua_ls = {
+                settings = {
                     Lua = {
                         workspace = { checkThirdParty = false },
                         telemetry = { enable = false },
                     },
                 },
-                pyright = {},
-                jsonls = {},
-                marksman = {},
-                dockerls = {},
-                clangd = {},
-                cmake = {},
-                jdtls = {},
-                ltex = {},
-                rust_analyzer = {},
-                yamlls = {},
-            }    
+            },
+            pyright = {},
+            jsonls = {},
+            marksman = {},
+            dockerls = {},
+            clangd = {},
+            cmake = {},
+            jdtls = {},
+            ltex = {},
+            rust_analyzer = {},
+            yamlls = {},
+        }
+        local on_attach = function(_, bufnr)
+            end
+        local capabilities = require('cmp_nvim_lsp').default_capabilities()
         require("mason-lspconfig").setup({
-                ensure_installed = vim.tbl_keys(servers),
-                handlers = {
-                    function(server_name)
-                        require("lspconfig")[server_name].setup {
-                            settings = servers[server_name],
-                            on_attach = on_attach,
-                        }
-                    end
-                }
-            })
+            ensure_installed = vim.tbl_keys(servers),
+        })
+
+        for servers, config in pairs(servers) do
+            require("lspconfig")[server].setup(
+                vim.tbl_deep_extend("keep",
+                    {
+                        on_attach = on_attach,
+                        capabilities = capabilities
+                    },
+                    config
+                )
+            )
+        end
     end,
 
 }
+
