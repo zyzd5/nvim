cb88754c69f9f9119afa9bf788ef8ad8927d74ad
Mac-zyzds <Mac-zyzds>
2024-04-20 16:09:09 +0800

    restructed lsp.lua structure

--- lua/plugins/lsp/lsp.lua
+++ lua/plugins/lsp/lsp.lua
@@ -1,29 +1,17 @@
 return
 {
     "neovim/nvim-lspconfig",
-    cmd = {
-            "Mason",
-            "Neoconf",
-        },
     dependencies = {
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig",
-        "folke/neoconf.nvim",
-        "folke/neodev.nvim",
-        {
-            "j-hui/fidget.nvim",
-            tag = "legacy",
-        },
-        "nvimdev/lspsaga.nvim",
+
     },
     config = function()
         local servers = {
             lua_ls = {
-                settings = {
-                    Lua = {
-                        workspace = { checkThirdParty = false },
-                        telemetry = { enable = false },
-                    },
+                Lua = {
+                    workspace = { checkThirdParty = false },
+                    telemetry = { enable = false },
                 },
             },
             pyright = {},
@@ -37,25 +25,34 @@ return
             rust_analyzer = {},
             yamlls = {},
         }
-        local on_attach = function(_, bufnr)
-            end
-        local capabilities = require('cmp_nvim_lsp').default_capabilities()
+        local on_attach = function(_, bufnf)
+            vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+            -- not workable, find a time add them to ketmap.lua
+            --             nmap('<leader>I', vim.lsp.buf.implementation, '[I]mplementation')
+            --             nmap('<C-K>', vim.lsp.buf.signature_help, 'Signature Documentation')
+            --             nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+            --             nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+            --             nmap('<leader>wl', function()
+            --                 print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+            --             end, '[W]orkspace [L]ist Folders')
+            -- nmap('<leader>wD', vim.lsp.buf.type_definition, 'Type [D]efinition')
+            --             nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[N]ame')
+            --             nmap('gr', vim.lsp.buf.references, '[G]oto [R]eferences')
+            --             nmap('<leader>F', function()
+            --                 vim.lsp.buf.format { async = true }
+            --             end, "[F]ormat code")
+        end
+        require('mason').setup()
         require("mason-lspconfig").setup({
             ensure_installed = vim.tbl_keys(servers),
-        })
-
-        for servers, config in pairs(servers) do
-            require("lspconfig")[server].setup(
-                vim.tbl_deep_extend("keep",
-                    {
+            handlers = {
+                function(server_name)
+                    require("lspconfig")[server_name].setup {
+                        settings = servers[server_name],
                         on_attach = on_attach,
-                        capabilities = capabilities
-                    },
-                    config
-                )
-            )
-        end
-    end,
-
+                    }
+                end,
+            }
+        })
+    end
 }
-
