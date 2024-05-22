a43eea53a6f7562c3dafc54dca2c9d0535c4dc6a
Mac-zyzds <Mac-zyzds>
2024-05-23 05:28:52 +0800

    added treesitter.lua

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,15 @@
+return
+{
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        local configs = require("nvim-treesitter.configs")
+
+        configs.setup({
+            ensure_installed = "all",
+            auto_install = true,
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
+    end
+}
