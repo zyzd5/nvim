09b4355588333d0793cbca6b2b3732a1901dc8d4
Mac-zyzds <Mac-zyzds>
2024-07-20 10:15:29 +0800

    basically complete lsp config

--- lua/plugins/dashboard-nvim.lua
+++ lua/plugins/dashboard-nvim.lua
@@ -1,21 +1,20 @@
 return
-{
-  'nvimdev/dashboard-nvim',
-  event = 'VimEnter',
-  config = function()
-    require('dashboard').setup {
-    theme = 'hyper',
-    config = {
-      week_header = {
-       enable = true,
-      },
-      shortcut = {
-        { desc = ' Update', group = '@property', action = 'Lazy update', key = 'u' },
-        { desc = '󰹰 Mason', group = '@property', action = 'Mason', key = 'M'},
-        { desc = '󰹯 Lazy', group = '@property', action = 'Lazy', key = 'L'},
-        { icon = ' ', icon_hl = '@variable', desc = 'Files', group = 'Label', action = 'Telescope find_files', key = 'f' },
-      },
+    {
+        'nvimdev/dashboard-nvim',
+        event = 'VimEnter',
+        enabled = false,
+        config = function()
+            require('dashboard').setup {
+                theme = 'hyper',
+                config = {
+                    week_header = { enable = true, },
+                    shortcut = {
+                        { desc = ' Update', group = '@property', action = 'Lazy update', key = 'u' },
+                        { desc = '󰹰 Mason', group = '@property', action = 'Mason', key = 'M'},
+                        { desc = '󰹯 Lazy', group = '@property', action = 'Lazy', key = 'L'},
+                        { icon = ' ', icon_hl = '@variable', desc = 'Files', group = 'Label', action = 'Telescope find_files', key = 'f' },
+                    },
+                }
+            }end,
+        dependencies = {{'nvim-tree/nvim-web-devicons'}}
     }
-    }end,
-  dependencies = {{'nvim-tree/nvim-web-devicons'}}
-}
--- lua/plugins/gruvbox-nvim.lua
+++ lua/plugins/gruvbox-nvim.lua
@@ -3,5 +3,20 @@ return
     "ellisonleao/gruvbox.nvim",
     priority = 1000,
     opts = ...,
-    event = "VeryLazy"
+    event = "VeryLazy",
+    config = function ()
+        vim.cmd [[colorscheme gruvbox]]
+        if vim.g.neovide then
+            vim.o.guifont = "UbuntuMono Nerd Font:h20:w1.1"
+            vim.o.linespace = 3
+            vim.g.neovide_scale_factor = 1.0
+            vim.g.neovide_transparency = 0.70
+            vim.g.neovide_show_border = true
+            vim.g.neovide_fullscreen = false
+            vim.g.neovide_no_window_frame = true
+        else
+            vim.cmd('hi Normal guibg=NONE ctermbg=NONE')
+        end
+        
+    end,
 }
