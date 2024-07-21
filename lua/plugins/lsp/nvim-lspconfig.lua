244b2e4974ed4a5e11e154d8fd1b2f5546087c59
Mac-zyzds <Mac-zyzds>
2024-07-22 06:43:04 +0800

    which-key.nvim added

--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -16,6 +16,5 @@ return {
 
 		vim.keymap.set("n", "K", vim.lsp.buf.hover, { noremap = true })
 		vim.keymap.set("n", "gd", vim.lsp.buf.definition, { noremap = true })
-		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { noremap = true })
 	end,
 }
