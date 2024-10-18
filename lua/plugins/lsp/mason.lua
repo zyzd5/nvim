4dee0c85860a99014b77173327499980fdd51bbc
Mac-zyzds <Mac-zyzds>
2024-10-19 02:12:14 +0800

    vscode-nvim disable

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -2,21 +2,15 @@ return {
 	{
 		"williamboman/mason.nvim",
 		config = function()
-            if vim.g.vscode then
-            else
-                require("mason").setup()
-            end
+            require("mason").setup()
 		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
-            if vim.g.vscode then
-            else
-                require("mason-lspconfig").setup({
-                    ensure_installed = { "lua_ls" },
-                })
-            end
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls" },
+			})
 		end,
 	},
 	{
@@ -24,33 +18,30 @@ return {
 		cmd = { "Mason", "Neoconf" },
 		event = { "BufReadPost", "BufNewFile" },
 		config = function()
-            if vim.g.vscode then
-            else
-                local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-                local lspconfig = require("lspconfig")
+			local lspconfig = require("lspconfig")
 
-                lspconfig.lua_ls.setup({
-                    capabilities = capabilities,
-                })
-                lspconfig.clangd.setup({
-                    capabilities = capabilities,
-                })
-                lspconfig.pyright.setup({
-                    capabilities = capabilities,
-                })
-                lspconfig.marksman.setup({
-                    capabilities = capabilities,
-                })
-                lspconfig.kotlin_language_server.setup({
-                    capabilities = capabilities,
-                })
-                lspconfig.cmake.setup({
-                    capabilities = capabilities,
-                })
-                vim.keymap.set("n", "K", vim.lsp.buf.hover, { noremap = true })
-                vim.keymap.set("n", "gd", vim.lsp.buf.definition, { noremap = true })
-            end
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.clangd.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.marksman.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.kotlin_language_server.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.cmake.setup({
+				capabilities = capabilities,
+			})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, { noremap = true })
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { noremap = true })
 		end,
 	},
 }
