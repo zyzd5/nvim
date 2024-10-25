30ed18eef6da1e2a2999e7d52afaa858ab809123
Mac-zyzds <Mac-zyzds>
2024-10-25 13:03:47 +0800

    default c++ format style for WebKit

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,17 +1,16 @@
-return
-{
+return {
     "nvimtools/none-ls.nvim",
     event = { "BufReadPost", "BufNewFile" },
     config = function()
         local null_ls = require("null-ls")
         null_ls.setup({
             sources = {
-                null_ls.builtins.formatting.stylua,
-            }
+                -- null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.clang_format,
+            },
         })
 
-        vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
-        vim.keymap.set('v', '<leader>gf', vim.lsp.buf.format, {})
-    end
-
+        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+        vim.keymap.set("v", "<leader>gf", vim.lsp.buf.format, {})
+    end,
 }
