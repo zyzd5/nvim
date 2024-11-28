bf4bbfbd193d5eb2a9f747b3c7c1b21f9fc22480
Mac-zyzds <Mac-zyzds>
2024-11-28 17:07:16 +0800

    remove render-markdown.nvim

--- lua/plugins/render-markdown.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"MeanderingProgrammer/render-markdown.nvim",
-    enabled = false,
-	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-	---@module 'render-markdown'
-	---@type render.md.UserConfig
-	opts = {},
-}
