a1f9d00601b023573e4226cf727b5f0c99751660
Mac-zyzds <Mac-zyzds>
2025-02-24 18:26:54 +0800

    Opacity command for ghostty and alacritty

--- lua/core/functions.lua
+++ lua/core/functions.lua
@@ -1,3 +1,68 @@
+local function alacritty_opacity(opt)
+	local file = io.open("/Users/zyzds/.config/alacritty/alacritty.toml", "r+")
+	if not file then
+		print("open file failed")
+		return
+	end
+
+	local offset = 0
+	while true do
+		local line = file:read("*l")
+		if not line then
+			break
+		end
+		local match_start, match_end = line:find("opacity = .*")
+		if match_start then
+			file:seek("set", offset + match_start - 1)
+			file:write("opacity = " .. tostring(opt.args))
+			break
+		end
+		offset = offset + #line + 1
+	end
+	file:close()
+end
+local function ghostty_opacity(opt)
+	local file = io.open("/Users/zyzds/Library/Application Support/com.mitchellh.ghostty/config", "r+")
+
+	if not file then
+		print("open file failed")
+		return
+	end
+
+	local opacity = tostring(opt.args)
+	if opt.args == "1" then
+		opacity = "1.0"
+	end
+
+	local offset = 0
+	while true do
+		local line = file:read("*l")
+		if not line then
+			break
+		end
+		local match_start, match_end = line:find(".*opacity = .*")
+		if match_start then
+			file:seek("set", offset + match_start - 1)
+			file:write("background-opacity = " .. opacity)
+			break
+		end
+		offset = offset + #line + 1
+	end
+	file:close()
+    vim.cmd(":silent !osascript ~/.config/nvim/lua/core/ghostty_opacity.scpt")
+end
+vim.api.nvim_create_user_command("Opacity", function(opts)
+	if os.getenv("ALACRITTY_WINDOW_ID") then
+		alacritty_opacity(opts)
+	elseif os.getenv("GHOSTTY_BIN_DIR") then
+        ghostty_opacity(opts)
+	end
+end, {
+	nargs = 1,
+	complete = function()
+		return { "0.8", "1.0" }
+	end,
+})
 vim.api.nvim_create_user_command("Code", function()
 	vim.cmd("!code %")
 end, {})
@@ -42,39 +107,6 @@ vim.api.nvim_create_user_command("Dark", function()
 	vim.cmd("let g:gruvbox_material_transparent_background = 2")
 	vim.cmd("colo gruvbox-material")
 end, {})
-vim.api.nvim_create_user_command("Opacity", function(opts)
-	local file = io.open("/Users/zyzds/.config/alacritty/alacritty.toml", "r+")
-	if not file then
-		print("open file failed")
-        return
-	end
-
-	local opacity = tostring(opts.args)
-	if opts.args == "1" then
-		opacity = "1.0"
-	end
-
-	local offset = 0
-	while true do
-		local line = file:read("*l")
-		if not line then
-			break
-		end
-		local match_start, match_end = line:find("opacity = %d+%.%d+")
-		if match_start then
-			file:seek("set", offset + match_start - 1)
-			file:write("opacity = " .. opacity)
-			break
-		end
-		offset = offset + #line + 1
-	end
-	file:close()
-end, {
-	nargs = 1,
-	complete = function()
-		return { "0.8", "1.0" }
-	end,
-})
 -- keybinding
 vim.keymap.set("n", "<F6>", ":CompileAndRun<CR>", {})
 vim.keymap.set("n", "r", ":CompileAndRun<CR>", {})
--- lua/plugins/barbecue.lua
+++ lua/plugins/barbecue.lua
@@ -3,6 +3,7 @@ return
     "utilyre/barbecue.nvim",
     name = "barbecue",
     version = "*",
+    enabled = true,
     dependencies = {
         "SmiteshP/nvim-navic",
         "nvim-tree/nvim-web-devicons", -- optional dependency
