5f2d3d297ff4ec0131ae4e77192cb3067dca3a14
Mac-zyzds <Mac-zyzds>
2025-03-15 17:55:29 +0800

    catppuccin.nvim default

--- lua/core/colorscheme.lua
+++ lua/core/colorscheme.lua
@@ -22,7 +22,8 @@ vim.cmd("let g:gruvbox_material_diagnostic_line_highlight = 1")
 vim.cmd("let g:gruvbox_material_diagnostic_virtual_text = 'colored'")
 vim.cmd("let g:gruvbox_material_better_performance = 1")
 
-vim.cmd.colorscheme("gruvbox-material")
+-- vim.cmd.colorscheme("gruvbox-material")
+vim.cmd.colorscheme("catppuccin")
 
 -- if vim.g.neovide then
 -- 	vim.o.guifont = "UbuntuMono Nerd Font:h20:w1.1"
