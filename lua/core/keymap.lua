5f92135d113ac18c9b24b034794cf504071ad4aa
Mac-zyzds <Mac-zyzds>
2025-08-24 10:19:50 +0800

    simplify open or close fold shortcut

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -36,9 +36,9 @@ keymap("i", "<D-BS>", "<C-w>", { noremap = true, silent = true })
 
 -- fold toggle
 keymap("n", "<leader>[", "zc", { silent = true })
-keymap("v", "<leader>f[", "zC", { silent = true })
+keymap("v", "<leader>[", "zC", { silent = true })
 keymap("n", "<leader>]", "zo", { silent = true })
-keymap("v", "<leader>f]", "zO", { silent = true })
+keymap("v", "<leader>]", "zO", { silent = true })
 
 -- <leader>w close current buffer
 keymap("n", "<leader>w", "<cmd>bd<CR>", { silent = true })
